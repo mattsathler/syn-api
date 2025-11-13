a28abcb9e4aaee0b5aedfd04edf80de044080b07
Matheus Sathler <mattsathler@gmail.com>
2025-11-13 15:02:42 -0300

    fix: add cors to synaps vercel

--- src/main.ts
+++ src/main.ts
@@ -3,15 +3,15 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  const allowedOrigins = [
+    'https://synapse-taupe-mu.vercel.app',
+    'http://localhost:4200',
+  ];
+
   app.enableCors({
-    origin: 'http://localhost:4200',
-    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
+    origin: allowedOrigins,
     credentials: true,
-  });
-  app.enableCors({
-    origin: 'https://synapse-taupe-mu.vercel.app/',
-    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
-    credentials: true,
+    methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH', 'OPTIONS'],
   });
 
   await app.listen(process.env.PORT ?? 3000);
